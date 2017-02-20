76dc0b36942c30c90f3fd59acc5e1bfbc1eeea22
Ligiano <ldaazzi@gmail.com>
2017-02-20 15:32:34 -0300

    class 4 (Ordenando e limitando paginação através da api)

--- laravel/app/Http/Controllers/Api/BanksController.php
+++ laravel/app/Http/Controllers/Api/BanksController.php
@@ -7,10 +7,24 @@ use App\Http\Controllers\Controller;
 
 class BanksController extends Controller
 {
-  function index(){
-    $result = \App\Bank::paginate();
+  public function index(Request $request)
+  {
+    $request['limit'] ? $limit = $request['limit'] : $limit = 15;
+    $request['order'] ? $order = $request['order'] : $order = null;
+
+    if ($order != null){
+      $order = explode(',', $order);
+    }
+
+    //$order[0] = $order ?? 'id';
+    $order[0] ? $order[0] : 'id'; // if do not receive position 0, then assume as id
+    $order[1] ? $order[1] : 'asc'; // if do not receive position 1, then assume as asc
+
+    $result = \App\Bank::orderBy($order[0], $order[1])
+      ->paginate($limit);
 
     return response()->json($result);
+
   }
 
 }
