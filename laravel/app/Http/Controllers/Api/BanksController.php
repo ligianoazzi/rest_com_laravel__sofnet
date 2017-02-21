0d85b27df3e3a4d7835a1ef33c12247da4f28c2a
Ligiano <ldaazzi@gmail.com>
2017-02-21 09:13:21 -0300

    class five (filter data)

--- laravel/app/Http/Controllers/Api/BanksController.php
+++ laravel/app/Http/Controllers/Api/BanksController.php
@@ -9,18 +9,51 @@ class BanksController extends Controller
 {
   public function index(Request $request)
   {
+
+    /*
+          examples of urls
+          http://localhost:8000/api/banks?where[id]=13&order=id,asc
+          http://localhost:8000/api/banks?where[id]=13
+          http://localhost:8000/api/banks?order=id,asc
+          http://localhost:8000/api/banks?like=title,Caixa
+    */
+
     $request['limit'] ? $limit = $request['limit'] : $limit = 15;
     $request['order'] ? $order = $request['order'] : $order = null;
 
-    if ($order != null){
+    if ($order){
       $order = explode(',', $order);
+    }else{
+      $order[0] = 'id';
+      $order[1] = 'asc';
     }
 
-    //$order[0] = $order ?? 'id';
-    $order[0] ? $order[0] : 'id'; // if do not receive position 0, then assume as id
-    $order[1] ? $order[1] : 'asc'; // if do not receive position 1, then assume as asc
+    if ( $request['where']){
+      $where = $request->all()['where'];
+    }else{
+      $where = [];
+    }
+
+
+    if($request['like']){
+      $like = $request['like'];
+    }else{
+      $like = null;
+    }
+
+    if($like){
+      $like = explode(',', $like);
+      $like[1] = '%' . $like[1] . '%';
+    }
 
     $result = \App\Bank::orderBy($order[0], $order[1])
+      ->where(function($query) use($like){
+        if($like){
+          return $query->where($like[0], 'like', $like[1]);
+        }
+        return $query;
+      })
+      ->where($where)
       ->paginate($limit);
 
     return response()->json($result);
