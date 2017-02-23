5ee53598f50c98838ac8f974284031fb0a7bed80
Ligiano <ldaazzi@gmail.com>
2017-02-23 10:33:06 -0300

    class eleven (allowed access external)

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -12,10 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware'=>['auth:api']], function (){
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+  Route::middleware('auth:api')->get('/user', function (Request $request) {
+      return $request->user();
+  });
 
-Route::resource('/banks', 'Api\BanksController');
-Route::resource('/accounts', 'Api\AccountsController');
+  Route::resource('/banks', 'Api\BanksController');
+  Route::resource('/accounts', 'Api\AccountsController');
+
+});  
